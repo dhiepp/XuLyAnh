c21790fe1f8a2f9e5e33e003c6756828472e3327
NGUYEN NGOC CUONG <cuong001998@gmai.com>
2019-11-06 13:56:59 +0700

    cuong

--- app/src/main/java/com/dhiep/android/xulyanh/fragments/FilterFragment.java
+++ app/src/main/java/com/dhiep/android/xulyanh/fragments/FilterFragment.java
@@ -1,6 +1,7 @@
 package com.dhiep.android.xulyanh.fragments;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,12 +10,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.dhiep.android.xulyanh.EditActivity;
 import com.dhiep.android.xulyanh.R;
+import com.dhiep.android.xulyanh.bitmap.BitmapProcessing;
 
-public class FilterFragment extends Fragment {
+public class FilterFragment extends Fragment implements View.OnClickListener{
+
     private EditActivity editActivity;
+    private Bitmap editBitmap;
+
+    private Button gaussian;
+    private Button grayScale;
+    private Button invert;
+    private Button noise;
+    private Button sepia;
+    private Button sharpen;
+    private Button vignette;
+
 
     public FilterFragment(EditActivity editActivity) {
         this.editActivity = editActivity;
@@ -24,7 +38,64 @@ public class FilterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filter, container, false);
+        View root =  inflater.inflate(R.layout.fragment_filter, container, false);
+
+        gaussian = root.findViewById(R.id.filter_gaussian);
+        grayScale = root.findViewById(R.id.filter_grayScale);
+        invert = root.findViewById(R.id.filter_invert);
+        noise = root.findViewById(R.id.filter_noise);
+        sepia = root.findViewById(R.id.filter_sepia);
+        sharpen = root.findViewById(R.id.filter_sharpen);
+        vignette = root.findViewById(R.id.filter_vignette);
+
+        gaussian.setOnClickListener(this);
+        grayScale.setOnClickListener(this);
+        invert.setOnClickListener(this);
+        noise.setOnClickListener(this);
+        sepia.setOnClickListener(this);
+        sharpen.setOnClickListener(this);
+        vignette.setOnClickListener(this);
+
+        return root;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        editBitmap = editActivity.getBitmap();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+       if(view.equals(gaussian)){
+           editBitmap = BitmapProcessing.gaussian(editBitmap);
+           editActivity.setBitmap(editBitmap);
+       }
+        if(view.equals(grayScale)){
+            editBitmap = BitmapProcessing.grayscale(editBitmap);
+            editActivity.setBitmap(editBitmap);
+        }
+        if(view.equals(invert)){
+            editBitmap = BitmapProcessing.invert(editBitmap);
+            editActivity.setBitmap(editBitmap);
+        }
+        if(view.equals(noise)){
+            editBitmap = BitmapProcessing.noise(editBitmap);
+            editActivity.setBitmap(editBitmap);
+        }
+        if(view.equals(sepia)){
+            editBitmap = BitmapProcessing.sepia(editBitmap);
+            editActivity.setBitmap(editBitmap);
+        }
+        if(view.equals(sharpen)){
+            editBitmap = BitmapProcessing.sharpen(editBitmap);
+            editActivity.setBitmap(editBitmap);
+        }
+        if(view.equals(vignette)){
+            editBitmap = BitmapProcessing.vignette(editBitmap);
+            editActivity.setBitmap(editBitmap);
+        }
+
+    }
 }
